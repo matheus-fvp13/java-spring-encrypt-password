21e631f5c98a62fb810dbd79ab81f2669ae9271a
Matheus Vieira <matheus.fvp@outlook.com>
2024-04-25 10:33:16 -0300

    feat - password encoder created

--- src/main/java/edu/mfvp/encryptpassword/EncryptPasswordApplication.java
+++ src/main/java/edu/mfvp/encryptpassword/EncryptPasswordApplication.java
@@ -3,12 +3,20 @@ package edu.mfvp.encryptpassword;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EncryptPasswordApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EncryptPasswordApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/edu/mfvp/encryptpassword/controllers/UserController.java
+++ src/main/java/edu/mfvp/encryptpassword/controllers/UserController.java
@@ -2,20 +2,24 @@ package edu.mfvp.encryptpassword.controllers;
 
 import edu.mfvp.encryptpassword.repositories.UserRepository;
 import edu.mfvp.encryptpassword.schemas.UserSchema;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping
@@ -25,11 +29,27 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<Void> createUser(@RequestBody UserSchema user, UriComponentsBuilder ucBuilder) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         var newUser = userRepository.save(user);
         var uri = ucBuilder
                 .path("/users/{id}")
                 .buildAndExpand(newUser.getId())
                 .toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/auth")
+    public ResponseEntity<Boolean> validatePassword(@RequestParam String username,
+                                                    @RequestParam String password) {
+        Optional<UserSchema> user = this.userRepository.findByUsername(username);
+
+        if(user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+        boolean valid = passwordEncoder.matches(password, user.get().getPassword());
+        int status = valid ? HttpStatus.OK.value() : HttpStatus.UNAUTHORIZED.value();
+
+        return ResponseEntity.status(status).body(valid);
     }
 }
